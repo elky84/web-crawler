8e39dd0edb7145d033173ccd576bb4ae2ba47311
elky <elkykim@gmail.com>
2020-11-09 13:00:02 +0900

    예외 로그 보강

--- Server/Exception/ExceptionExtend.cs
+++ Server/Exception/ExceptionExtend.cs
@@ -10,19 +10,22 @@ namespace Server.Exception
             if (e.InnerException?.GetType() == typeof(DeveloperException))
             {
                 var developerException = (DeveloperException)e.InnerException;
-                Log.Logger.Error($"[Exception] LoopingService [Message:{developerException.Message}], [ResultCode:{developerException.ResultCode}]");
-                Log.Logger.Error($"[StrackTrace] LoopingService [{developerException.StackTrace}]");
+                Log.Logger.Error($"[Exception] [Message:{developerException.Message}], [ResultCode:{developerException.ResultCode}]");
+                Log.Logger.Error($"[Exception] [Source:{e.Source}], [InnerSource:{developerException.Source}]");
+                Log.Logger.Error($"[StrackTrace] [{e.StackTrace}]");
+                Log.Logger.Error($"[InnerStrackTrace] [{developerException.StackTrace}]");
             }
             else if (e.InnerException != null)
             {
-                Log.Logger.Error($"[Exception] LoopingService [Message:{e.Message}] [InnerMessage:{e.InnerException.Message}]");
-                Log.Logger.Error($"[StrackTrace] LoopingService [{e.InnerException.StackTrace}]");
-                Log.Logger.Error($"[InnerStrackTrace] LoopingService [{e.StackTrace}]");
+                Log.Logger.Error($"[Exception] [Message:{e.Message}] [InnerMessage:{e.InnerException.Message}]");
+                Log.Logger.Error($"[Exception] [Source:{e.Source}] [InnerSource:{e.InnerException.Source}]");
+                Log.Logger.Error($"[StrackTrace] [{e.StackTrace}]");
+                Log.Logger.Error($"[InnerStrackTrace] [{e.InnerException.StackTrace}]");
             }
             else
             {
-                Log.Logger.Error($"[Exception] LoopingService [Message:{e.Message}]");
-                Log.Logger.Error($"[StrackTrace] LoopingService [{e.StackTrace}]");
+                Log.Logger.Error($"[Exception] [Message:{e.Message}] [Source:{e.Source}]");
+                Log.Logger.Error($"[StrackTrace] [{e.StackTrace}]");
             }
         }
     }
